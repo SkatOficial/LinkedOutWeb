25aec8889047c8f5d76f8100a90630431065a19d
Sebastian Abarca <sebastian.abarca.p@gmail.com>
2024-12-06 18:01:21 -0300

    agregacion animacion de boton

--- home/static/js/animations.js
+++ home/static/js/animations.js
@@ -1,18 +1,19 @@
 const images = document.querySelectorAll('[class*="anim-"]');
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-        }else{
-          entry.target.classList.remove('visible');
-        }
-      });
-    }, {
-        root: null,
-        threshold: 0.3, 
-      });
+// ANIMACION DESPLAZAMIENTOS
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }else{
+      entry.target.classList.remove('visible');
+    }
+  });
+}, {
+    root: null,
+    threshold: 0.3, 
+  });
 
-    images.forEach(image => {
-      observer.observe(image);
-    });
+images.forEach(image => {
+  observer.observe(image);
+});
