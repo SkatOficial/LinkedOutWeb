aff455ae6aa68760201a2b50b1b6e1eafafe3841
Sebastian Abarca <sebastian.abarca.p@gmail.com>
2024-12-06 06:22:59 -0300

    arreglo de otro error ortografico (nose escribir)

--- LinkedOutWeb/settings.py
+++ LinkedOutWeb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviro.get('SECRET_KEY',default='django-insecure-(ek4_(-2eunp9ddqtxp8y52tsbmh)!ph(!8s5b4t**m#t+b$jj')
+SECRET_KEY = os.environ.get('SECRET_KEY',default='django-insecure-(ek4_(-2eunp9ddqtxp8y52tsbmh)!ph(!8s5b4t**m#t+b$jj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
