ee9ea61d823607f553763d80a072dec604d33663
Sebastian Abarca <sebastian.abarca.p@gmail.com>
2024-12-14 20:21:16 -0300

    avance navbar responisivo, se movio los archivos boostrap a la raiz

--- core/static/js/nav.js
+++ core/static/js/nav.js
@@ -1,3 +1,4 @@
+//------------CAMBIA EL NAV A FIXED------------
 const navbar = document.getElementById('nav');
 const placeholder = document.createElement('div');
 
@@ -18,3 +19,54 @@ window.addEventListener('scroll', () => {
     nav.removeAttribute('data-fixed'); 
     placeholder.style.display = 'none';
   }});
+
+//------------EVENTO CLICK DROP MENU------------
+document.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+  const parentElement = clickedElement.parentElement;
+
+  if(clickedElement.classList.contains("title-drop-menu")){
+    if(parentElement.getAttribute('data-active') == 'true'){
+      parentElement.setAttribute('data-active', 'false');
+    }else{
+      resetDataActive();
+      parentElement.setAttribute('data-active', 'true');
+    }
+  }
+});
+
+//------------EVENTO CLICK DESPLEGAR MENU------------
+const buttonMenu = document.getElementById("button-menu")
+
+buttonMenu.addEventListener("click",(event) => {
+  const navElement = document.querySelector('.nav ul')
+  if (navElement.style.height != "auto") {
+    navElement.style.height = "auto";
+  } else {
+    navElement.style.height = "0px";
+  }
+
+})
+//------------REINICIO DE DATA-ACTIVE ------------
+function resetDataActive(){
+  const elementsData = document.getElementsByClassName("container-drop-menu");
+
+  for (let i = 0; i < elementsData.length; i++) {
+    elementsData[i].setAttribute('data-active', 'false');
+  }
+}
+
+//------------ REINICIO NAV ------------
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+function handleResize(event) {
+  const navElement = document.querySelector('.nav ul')
+
+  if (event.matches) {
+    resetDataActive();
+    navElement.removeAttribute('style')
+  }
+}
+
+handleResize(mediaQuery);
+mediaQuery.addEventListener("change", handleResize);
