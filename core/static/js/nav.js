0f53a5b1c949ff6608617b51acc486991ae4a2c5
Sebastian Abarca <sebastian.abarca.p@gmail.com>
2024-12-28 15:19:51 -0300

    colocacion de comentarios a archivo 'nav.js' y ordenamiento

--- core/static/js/nav.js
+++ core/static/js/nav.js
@@ -1,51 +1,69 @@
+//------------EJECUTA FUNCIONES------------
+navfixed()
+displayDropMenu()
+displayMenu()
+resetNav()
+addArrowsDropMenus()
+
 //------------CAMBIA EL NAV A FIXED------------
-const navbar = document.getElementById('nav');
-const placeholder = document.createElement('div');
-
-placeholder.className = 'placeholder';
-nav.parentNode.insertBefore(placeholder, nav);
-
-const navHeight = nav.offsetHeight;
-const navOffsetTop = nav.offsetTop - 20; //se restan los margin o padding que hagan funcionar mal el efecto
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY >= navOffsetTop) {
-    nav.setAttribute('data-fixed', 'true');
-
-    placeholder.style.height = `${navHeight}px`;
-    placeholder.style.display = 'block';
-    
-  } else {
-    nav.removeAttribute('data-fixed'); 
-    placeholder.style.display = 'none';
-  }});
+/* cuando el nav llega a la parte superior de la pantalla se convierte a fixed*/
+function navfixed(){
+  const navbar = document.getElementById('nav');
+  const placeholder = document.createElement('div');
+  
+  placeholder.className = 'placeholder';
+  nav.parentNode.insertBefore(placeholder, nav);
+  
+  const navHeight = nav.offsetHeight;
+  const navOffsetTop = nav.offsetTop - 20; //se restan los margin o padding que hagan funcionar mal el efecto
+  
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= navOffsetTop) {
+      nav.setAttribute('data-fixed', 'true');
+  
+      placeholder.style.height = `${navHeight}px`;
+      placeholder.style.display = 'block';
+      
+    } else {
+      nav.removeAttribute('data-fixed'); 
+      placeholder.style.display = 'none';
+    }});
+}
 
 //------------EVENTO CLICK DROP MENU------------
-const dropMenus = document.querySelectorAll(".container-drop-menu")
+/* agrega el evento click que permite abrir y cerrar a cada dropmenu */
+function displayDropMenu(){
+  const dropMenus = document.querySelectorAll(".container-drop-menu")
 
-dropMenus.forEach(dropMenu => {
-  dropMenu.addEventListener("click", function() {
-    if(dropMenu.getAttribute('data-dropped') == 'true'){
-      dropMenu.setAttribute('data-dropped', 'false');
-    }else{
-      resetDataActive();
-      dropMenu.setAttribute('data-dropped', 'true');
-    }
-  });
-})
+  dropMenus.forEach(dropMenu => {
+    dropMenu.addEventListener("click", function() {
+      if(dropMenu.getAttribute('data-dropped') == 'true'){
+        dropMenu.setAttribute('data-dropped', 'false');
+      }else{
+        resetDataActive();
+        dropMenu.setAttribute('data-dropped', 'true');
+      }
+    });
+  })
+}
 
 //------------EVENTO CLICK DESPLEGAR MENU------------
-const buttonMenu = document.getElementById("button-menu")
+/* agrega el evento click al boton para desplegar el contenido del navbar*/
+function displayMenu(){
+  const buttonMenu = document.getElementById("button-menu")
 
-buttonMenu.addEventListener("click",(event) => {
-  const navElement = document.querySelector('.nav ul')
-  navElement.classList.toggle("open");
-  if(!navElement.classList.contains("open")){
-    resetDataActive();
-  }
-})
+  buttonMenu.addEventListener("click",(event) => {
+    const navElement = document.querySelector('.nav ul')
+    navElement.classList.toggle("open");
+    if(!navElement.classList.contains("open")){
+      resetDataActive();
+    }
+  })
+
+}
 
 //------------REINICIO DE TODOS LOS DATA-ACTIVE ------------
+/* cierra todos los dropmenus */
 function resetDataActive(){
   const elementsData = document.getElementsByClassName("container-drop-menu");
 
@@ -55,21 +73,28 @@ function resetDataActive(){
 }
 
 //------------ REINICIO NAV ------------
-const mediaQuery = window.matchMedia("(min-width: 768px)");
+/* Evalua si la pantalla tiene un minimo de 768px de ancho, 
+y reinicia los datos de los dropMenu y el nav */
+function resetNav(){
+  const mediaQuery = window.matchMedia("(min-width: 768px)");
 
-function handleResize(event) {
-  const navElement = document.getElementById('content-nav')
+  function handleResize(event) {
+    const navElement = document.getElementById('content-nav')
 
-  if (event.matches) {
-    resetDataActive();
-    navElement.removeAttribute('style')
+    if (event.matches) {
+      resetDataActive();
+      navElement.removeAttribute('class')
+    }
   }
+
+  handleResize(mediaQuery);
+  mediaQuery.addEventListener("change", handleResize);
 }
 
-handleResize(mediaQuery);
-mediaQuery.addEventListener("change", handleResize);
 
-//------------ Colocacion de felchas .svg en sus contenedores ------------
+//------------ Flechas DropMenus ------------
+/* agrega las flechas a cada DropMenu*/
+function addArrowsDropMenus(){
   const arrowUrl = "/static/img/arrow.svg";
 
   const containers = document.querySelectorAll('.arrow-container');
@@ -83,4 +108,6 @@ mediaQuery.addEventListener("change", handleResize);
               container.innerHTML = data;
           });
       })
+}
+  
 
